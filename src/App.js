8f9fd55eb2d78c353a36a9186ffcfb39bbe75e4c
Ishola Sodiq Olatunde <ishola.olatundey@gmail.com>
2024-10-16 11:13:17 +0100

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./containers/home";
+import Resume from "./containers/resume";
+import About from "./containers/about";
+import Contact from "./containers/contact";
+import Skills from "./containers/skills";
+import Portfolio from "./containers/portfolio";
+import NavBar from "./components/navBar";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* particles js */}
+
+      {/* navbar js */}
+      <NavBar/>
+      {/* main page content */}
+      <Routes>
+        <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/resume" element={<Resume />}/>
+          <Route path="/contact" element={<Contact />}/>
+          <Route path="/skills" element={<Skills />}/>
+          <Route path="/portfolio" element={<Portfolio />}/>
+        
+      </Routes>
     </div>
   );
 }
